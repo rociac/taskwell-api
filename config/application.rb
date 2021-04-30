2081fa4f80587a1463788112bf0fff9138e8256b
rociac <brsrkdev@gmail.com>
2021-04-30 11:54:36 -0500

    Fix secrets

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module TaskwellApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1 and config.autoloader = :classic
-    config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
+    config.secret_key_base = ENV["secret_key_base"]
 
     # Configuration for the application, engines, and railties goes here.
     #
